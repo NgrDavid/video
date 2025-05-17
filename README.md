ef5f0c8fe904e801c6e5bbbf8242f1101f2f3b04
David Maas <d.maas+git@neurogears.org>
2025-05-16 23:21:23 -0500

    Add CI/CD and brought repository into compliance with modern standards

--- README.md
+++
@@ -1,4 +0,0 @@
-Bonsai - Video Library
-----------------------
-
-A Bonsai Library containing reactive infrastructure to access various video sources using a DirectShow interface.
---
+++ src/Bonsai.Video/MjpegStream.cs
@@ -0,0 +1,27 @@
+﻿using AForge.Video;
+using System.ComponentModel;
+
+namespace Bonsai.Video
+{
+    [Description("Produces a sequence of images downloaded from the specified MJPEG stream.")]
+    public class MjpegStream : VideoCapture
+    {
+        [Description("The URL which will provide the MJPEG stream.")]
+        public string SourceUrl { get; set; }
+
+        [Description("The login required to access the video source.")]
+        public string Login { get; set; }
+
+        [Description("The password required to access the video source.")]
+        public string Password { get; set; }
+
+        protected override IVideoSource CreateVideoSource()
+        {
+            return new MJPEGStream(SourceUrl)
+            {
+                Login = Login,
+                Password = Password
+            };
+        }
+    }
+}
